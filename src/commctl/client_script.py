d89263bb9999ee6f5026abc4c799b1466c0f0712
Matthew Barnes <mbarnes@redhat.com>
2016-05-17 09:18:18 -0400

    Make passhash exclusive to commctl.
    Decided it's a poor fit for atomic at this time.
    
    Closes: #12
    Approved by: ashcrow

--- src/commctl/client_script.py
+++ src/commctl/client_script.py
@@ -18,9 +18,27 @@
 Client CLI for commissaire.
 """
 
+from __future__ import print_function
+
 import argparse
 
 import commctl.cli
+
+
+def do_passhash(args):
+    """
+    Uses bcrypt to hash a password.
+    """
+    import bcrypt
+    if args.password is not None:
+        pw = args.password
+    elif args.file is not None:
+        pw = args.file.read()
+    else:
+        import getpass
+        pw = getpass.getpass()
+    salt = bcrypt.gensalt(log_rounds=args.rounds)
+    return bcrypt.hashpw(pw, salt)
 
 
 def main():
@@ -30,7 +48,7 @@ def main():
     epilog = 'Example: commctl create upgrade datacenter1 -u 7.2.2'
 
     parser = argparse.ArgumentParser(epilog=epilog)
-    subparser = parser.add_subparsers()
+    subparser = parser.add_subparsers(dest='command')
 
     cluster_parser = subparser.add_parser('cluster')
     commctl.cli.add_cluster_commands(cluster_parser)
@@ -38,11 +56,28 @@ def main():
     host_parser = subparser.add_parser('host')
     commctl.cli.add_host_commands(host_parser)
 
+    # XXX passhash is more like a helper script.  Keep it out
+    #     of the shared API for now, and exclusive to commctl.
+    subcmd_parser = subparser.add_parser('passhash')
+    subcmd_parser.add_argument(
+        '-p', '--password', help='Password to hash')
+    subcmd_parser.add_argument(
+        '-f', '--file', type=argparse.FileType('rb'),
+        help='Password file to hash (or "-" for stdin)')
+    subcmd_parser.add_argument(
+        '-r', '--rounds', type=int, default=12, help='Number of rounds')
+
     args = parser.parse_args()
 
-    dispatcher = args._class()
-    dispatcher.set_args(args)
-    getattr(dispatcher, args.func)()
+    if args.command == 'passhash':
+        try:
+            print(do_passhash(args))
+        except Exception as ex:
+            parser.error(ex)
+    else:
+        dispatcher = args._class()
+        dispatcher.set_args(args)
+        getattr(dispatcher, args.func)()
 
 
 if __name__ == '__main__':  # pragma: no cover
